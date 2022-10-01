fc9725f1b9c1f132c0a2fbe7c48d938491282d52
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-10-01 17:16:35 +0300

    test react app has been created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${value} times`;
+  });
+
+  return (
+    <div>
+      <p>`You clicked {value} times`</p>
+      <button type="button" onClick={() => setValue(value + 1)}>
+        Increment value by 1
+      </button>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createContext } from "react";
 import "./index.css";
 import App from "./App";
 
+const MyContext = createContext();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <MyContext.Provider>
+    <App />
+  </MyContext.Provider>
+);
